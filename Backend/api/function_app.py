39c28f3b93f609122f3519081733368f34149bb1
Azeemudin47 <azeemuddin29220@gmail.com>
2024-05-08 16:31:08 +0300

    Initial Commit

---
+++ Backend/api/function_app.py
@@ -0,0 +1,58 @@
+
+import json
+import azure.functions as func
+from azure.cosmos import CosmosClient
+
+# Initialize CosmosClient with account endpoint and account key
+endpoint = "https://cosmosdbazureresume.documents.azure.com:443/"
+key = "dlLC6mtUVegAeJK3RSbGorv28VBzhEdeZFhJyZOBQN9I8LeRoS60IYBXsudOM7e0YRPCDZAXI3guACDbL24ilw=="
+client = CosmosClient(endpoint, credential=key)
+
+# Specify the name of your database and container
+database_name = "AzureResume"
+container_name = "Counter"
+item_id = "1"
+
+database = client.get_database_client(database_name)
+container = database.get_container_client(container_name)
+
+app = func.FunctionApp(http_auth_level=func.AuthLevel.FUNCTION)
+
+@app.route(route="GetResumeCounter")
+def GetResumeCounter(req: func.HttpRequest) -> func.HttpResponse:
+        
+    # Retrieve current count item
+    count_item = container.read_item(item_id, item_id)
+
+    # Validate the required parameters
+    if not count_item:
+        return func.HttpResponse(
+            "Count item missing",
+            status_code=400  # Bad Request
+        )
+    
+    # Extract the integer property from the item
+    current_count = count_item.get('count', 0)
+
+    # increment the count for each page refresh/API call
+    updated_count = current_count + 1
+
+    # Update the count item with the new value
+    count_item['count'] = updated_count
+    container.upsert_item(count_item)
+    
+    # Serialize the item to JSON
+    item_json = json.dumps(count_item, indent=2)
+
+    # Return the item as a JSON response
+    return func.HttpResponse(
+        body=item_json, 
+        mimetype='application/json',
+        status_code=200
+    )
+
+    
+
+    
+
+    
